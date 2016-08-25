c469d945aed5236c4477d7902f43b3e6ce0b72c7
Eran Keren <ekeren@mimecast.com>
2016-08-26 00:28:20 +0100

    travis ci changes

--- travis_after_success.sh
+++ travis_after_success.sh
@@ -8,7 +8,6 @@ echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
 echo "Travis JDK version:  " ${TRAVIS_JDK_VERSION}
 
 if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ]; then
-
     mvn clean --quiet
     if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
         SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|\w+:)')
